ac7f67640454d18eea5506c024a2443ed7763945
MasterJhonny <jhonnyeddy.22@gmail.com>
2021-05-27 11:36:36 -0400

    practica modules

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+//es7
+// includes
+let numbers = [1,2,3,4,5,6,7,8,9];
+let valorBus = 11
+if(numbers.includes(valorBus)){
+    console.log(`si, se encontro el valor ${valorBus}`)
+} else {
+    console.log('no. se encuntra el valor')
+}
+
+// pow
+let base = 3
+let expo = 3
+let ress = base ** expo
+console.log(`result: ${ress}`)
---
+++ src/es7/practica.js
@@ -0,0 +1,13 @@
+let valores = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
+
+let diaBus = 'feriado'
+
+if(valores.includes(diaBus)){
+    console.log('si')
+} else {
+    console.log('no')
+}
+
+// pow
+ 
+console.log(3**4)
